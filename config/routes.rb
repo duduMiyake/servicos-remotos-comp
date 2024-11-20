a60dd74cf2e2cadaf1fcaf024ee4b3c4e6ffe076
Eduardo Miyake <dudsmiyake@gmail.com>
2024-11-20 12:44:00 -0300

    update: fixed route playlist/add_music

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :usuarios
   resources :musicas
   resources :playlists do
-    post "musicas", to: "playlists#add_musicas"
+    post "musicas", to: "playlists#add_musicas", on: :member
   end
 end
